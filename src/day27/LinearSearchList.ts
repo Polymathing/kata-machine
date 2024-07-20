d211d50554b7849d2d38403813e8bef75c153925
Maicon Rodrigues <maiconrodriguessc@gmail.com>
2024-07-20 04:06:15 +0000

    day27

---
+++ src/day27/LinearSearchList.ts
@@ -0,0 +1,11 @@
+import { hasJSDocParameterTags } from "typescript";
+
+export default function linear_search(haystack: number[], needle: number): boolean {
+ for (let i = 0; i < haystack.length; ++i) {
+    if (needle === haystack[i]) {
+        return true;
+    }
+ }
+
+ return false;
+}
