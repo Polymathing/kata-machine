364e2cbf7268d8a12867f392faa589fda774af6a
Maicon Rodrigues <maiconrodriguessc@gmail.com>
2024-07-24 02:30:16 +0000

    day29

---
+++ src/day29/BubbleSort.ts
@@ -0,0 +1,16 @@
+export default function bubble_sort(arr: number[]): void {
+    let i = 0;
+
+    do {
+        let j = 0;
+        do {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+          ++j;
+        } while (j < arr.length -1 -i);
+        ++i;
+    } while (i < arr.length - 1)
+}
