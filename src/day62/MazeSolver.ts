1ff1dd5b4d7251674895ed5d2955b5ffd4dca1e8
Maicon Rodrigues <maiconrodriguessc@gmail.com>
2024-10-19 20:35:24 +0000

    day62

---
+++ src/day62/MazeSolver.ts
@@ -0,0 +1,60 @@
+const directions = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+];
+
+function canWalk(maze: string[], curr: Point, end: Point, wall: string, path: Point[], seen: boolean[][]) {
+    const currY = curr.y;
+    const currX = curr.x;
+
+    if (currY === end.y && currX === end.x) {
+        path.push(curr);
+        return true;
+    }
+
+    if (currY < 0 || currY > maze.length || 
+        currX < 0 || currX > maze[0].length
+    ) {
+        return false;
+    }
+
+    if (maze[currY][currX] === wall) {
+        return false;
+    }
+
+    if (seen[currY][currX]) {
+        return false;
+    }
+    
+    path.push(curr);
+    seen[currY][currX] = true;
+
+    for(let i = 0; i < maze.length; ++i) {
+        const [y, x] = directions[i];
+
+        if (canWalk(maze, {
+            y: currY + y,
+            x: currX + x
+        }, end, wall, path, seen)) {
+            return true;
+        }
+    }
+
+    path.pop();
+    return false;
+}
+
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+    const path: Point[] = [];
+    const seen: boolean[][] = [];
+
+    for(let i = 0; i < maze.length; ++i) {
+        seen.push(new Array(maze[i].length).fill(false));
+    }
+
+    canWalk(maze, start, end, wall, path, seen);
+
+    return path;
+}
