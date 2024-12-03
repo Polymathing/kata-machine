9ec66ccaafad250351c8f3f7f2e84927f441ba31
Maicon <maiconrodriguessc@gmail.com>
2024-12-03 19:58:11 -0300

    day81

---
+++ src/day81/BinarySearchList.ts
@@ -0,0 +1,21 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let l = 0;
+    let h = haystack.length;
+
+    while (h > l) {
+        const m = Math.floor(l + ((h - l ) / 2));
+        const v = haystack[m];
+
+        if (v === needle) {
+            return true;
+        }
+
+        if (v < needle) {
+            l = m + 1;
+        } else {
+            h = m;
+        }
+    }
+
+    return false;
+}
