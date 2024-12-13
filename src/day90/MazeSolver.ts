ad67bbc96fc8e4bee3960a8a34b57664fefbe9b0
Maicon <maiconrodriguessc@gmail.com>
2024-12-13 07:03:35 -0300

    day90

---
+++ src/day90/MazeSolver.ts
@@ -0,0 +1,54 @@
+const dir = [
+    [1,0],
+    [0,1],
+    [-1,0],
+    [0,-1]
+]
+
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]) {
+    const { y, x } = curr;
+    if (y === end.y && x === end.x) {
+        path.push(curr);
+        return true;
+    }
+
+    if (y < 0 || y > maze.length || x < 0 || x > maze[0].length) {
+        return false;
+    }
+
+    if (seen[y][x]) {
+        return false;
+    }
+
+    if (maze[y][x] === wall) {
+        return false;
+    }
+
+    path.push(curr);
+    seen[y][x] = true;
+
+    for(let i = 0; i < maze.length; ++i) {
+        const [dirY, dirX] = dir[i];
+        if (walk(maze, wall, {
+            y: y + dirY,
+            x: x + dirX
+        }, end, seen, path)) {
+            return true;
+        }
+    }
+
+    path.pop();
+    return false;
+}
+
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+
+    for(let i = 0; i < maze.length; ++i) {
+        seen.push(new Array(maze[i].length).fill(false));
+    }
+
+    walk(maze, wall, start, end, seen, path);
+    return path;
+}
