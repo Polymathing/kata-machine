22fd151318eeb56d9d1079f55f9a7eac1c71e57b
Maicon Santos <maiconrodriguessc@gmail.com>
2025-07-02 14:16:32 -0300

    day150

---
+++ src/day150/BTPostOrder.ts
@@ -0,0 +1,14 @@
+export default function post_order_search(head: BinaryNode<number>): number[] {
+  return traverse(head, []);
+}
+
+function traverse(head: BinaryNode<number> | null, path: number[]) {
+  if (!head) {
+    return path;
+  }
+
+  traverse(head.left, path);
+  traverse(head.right, path);
+  path.push(head.value);
+  return path;
+}
