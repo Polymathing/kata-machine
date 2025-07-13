1a7e6801ba1f128812e765bf31845de8e5c30213
Maicon Santos <maiconrodriguessc@gmail.com>
2025-07-13 15:31:18 -0300

    day154

--- ligma.config.js
+++ ligma.config.js
@@ -1,8 +1,7 @@
 module.exports = {
     dsa: [
-        "Queue",
-        "Stack",
-        "BTInOrder",
-        "BTBFS"
-    ]
+    "BTBFS",
+    "BinarySearchList",
+    "TwoCrystalBalls"
+]
 };
---
+++ src/day154/BTBFS.ts
@@ -0,0 +1,20 @@
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const queue: (BinaryNode<number> | null)[] = [head];
+
+  while (queue.length) {
+    const curr: BinaryNode<number> | undefined | null = queue.shift();
+
+    if (!curr) {
+      continue;
+    }
+
+    if (curr.value === needle) {
+      return true;
+    }
+
+    queue.push(curr.left);
+    queue.push(curr.right)
+  }
+
+  return false;
+}
